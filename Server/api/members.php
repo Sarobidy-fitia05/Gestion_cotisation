9aaf5476c12724e280b415ca9b7af2eed690e305
sarobidy <hei.sarobidy.6pgmail.com>
2026-04-28 14:28:53 +0300

    feat: Implement POST API for members with input validation and database insertion

--- Server/api/members.php
+++ Server/api/members.php
@@ -7,10 +7,50 @@ header("Content-Type: application/json");
 $database = new Database();
 $db = $database->connect();
 
-$query = "SELECT * FROM members";
-$stmt = $db->prepare($query);
-$stmt->execute();
+$method = $_SERVER['REQUEST_METHOD'];
 
-$members = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($method === "GET") {
+    $query = "SELECT * FROM members";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    
+    $members = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo json_encode($members);
 
-echo json_encode($members);
+}
+if ($method  === "POST") {
+    $data = json_decode(file_get_contents("php://input"));
+
+    if (
+        !isset($data->nom) || 
+        !isset($data->prenom) ||
+        !isset($data->telephone) ||
+        !isset($data->adresse) ||
+        !isset($data->poste)
+    ) {
+        echo json_encode([
+            "message"=>"donnée incompètes"
+        ]);
+        exit;
+    }
+    $query = "INSERT INTO members (nom, prenom, telephone, adresse, poste) VALUES (:nom, :prenom, :telephone, :adresse, :poste)";
+
+    $stmt = $db->prepare($query);
+
+    $stmt->bindParam("nom", $data->nom);
+    $stmt->bindParam("prenom", $data->prenom);
+    $stmt->bindParam("telephone", $data->telephone);
+    $stmt->bindParam("adresse", $data->adresse);
+    $stmt->bindParam("poste", $data->poste);
+
+    if ($stmt->execute()) {
+        echo json_encode([
+            "message" => "membre ajouté avec succès"
+        ]);
+    } else{
+        echo json_encode([
+            "message" => "Erreur lors de l'ajout"
+        ]);
+    }
+}
